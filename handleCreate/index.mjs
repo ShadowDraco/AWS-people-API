ed727855693f0a601de1de9d6a1036e1f18bcadf
Ethan Storm <stormethan10@gmail.com>
2023-06-08 16:46:44 -0700

    finished lab and documentation

--- handleCreate/index.mjs
+++ handleCreate/index.mjs
@@ -2,35 +2,35 @@ import dynamoose from 'dynamoose';
 
 // Create a dynamoose Schema
 const schema = new dynamoose.Schema({
-  id: String,
-  age: Number,
+	id: String,
+	age: Number,
 });
 
 // Create a model
 
-const friends = dynamoose.model('friends', schema);
+const people = dynamoose.model('people', schema);
 
 export const handler = async event => {
-  // create a response object
-  const response = {
-    statusCode: null,
-    body: null,
-  };
+	// create a response object
+	const response = {
+		statusCode: null,
+		body: null,
+	};
 
-  try {
-    // parse the event body so it can be used for creating
-    let parsedEventBody = JSON.parse(event.body);
+	try {
+		// parse the event body so it can be used for creating
+		let parsedEventBody = JSON.parse(event.body);
 
-    let results = await friends.create(parsedEventBody);
-    console.log('\n\n RESULTS FROM CREATE', results);
+		let results = await people.create(parsedEventBody);
+		console.log('\n\n RESULTS FROM CREATE', results);
 
-    // set response with results
-    response.body = results;
-    response.statusCode = 200;
-  } catch (err) {
-    response.body = JSON.stringify('ERROR AT CREATE:', err.message);
-    response.statusCode = 500;
-  }
+		// set response with results STRINGIFIED
+		response.body = JSON.stringify(results);
+		response.statusCode = 200;
+	} catch (err) {
+		response.body = JSON.stringify('ERROR AT CREATE:', err.message);
+		response.statusCode = 500;
+	}
 
-  return response;
+	return response;
 };
