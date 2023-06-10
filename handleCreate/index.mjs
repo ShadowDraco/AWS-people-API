7785fa91f10ddb471f3d295105eb8c9579897f16
Ethan Storm <stormethan10@gmail.com>
2023-06-09 22:36:26 -0700

    fix spacing

--- handleCreate/index.mjs
+++ handleCreate/index.mjs
@@ -1,36 +1,36 @@
-import dynamoose from 'dynamoose';
+import dynamoose from 'dynamoose'
 
 // Create a dynamoose Schema
 const schema = new dynamoose.Schema({
 	id: String,
 	age: Number,
-});
+})
 
 // Create a model
 
-const people = dynamoose.model('people', schema);
+const people = dynamoose.model('people', schema)
 
 export const handler = async event => {
 	// create a response object
 	const response = {
 		statusCode: null,
 		body: null,
-	};
+	}
 
 	try {
 		// parse the event body so it can be used for creating
-		let parsedEventBody = JSON.parse(event.body);
+		let parsedEventBody = JSON.parse(event.body)
 
-		let results = await people.create(parsedEventBody);
-		console.log('\n\n RESULTS FROM CREATE', results);
+		let results = await people.create(parsedEventBody)
+		console.log('\n\n RESULTS FROM CREATE', results)
 
 		// set response with results STRINGIFIED
-		response.body = JSON.stringify(results);
-		response.statusCode = 200;
+		response.body = JSON.stringify(results)
+		response.statusCode = 200
 	} catch (err) {
-		response.body = JSON.stringify('ERROR AT CREATE:', err.message);
-		response.statusCode = 500;
+		response.body = JSON.stringify('ERROR AT CREATE:', err.message)
+		response.statusCode = 500
 	}
 
-	return response;
-};
+	return response
+}
--- handleRead/index.mjs
+++ handleRead/index.mjs
@@ -1,56 +1,56 @@
-import dynamoose from 'dynamoose';
+import dynamoose from 'dynamoose'
 
 // Create a dynamoose Schema
 const schema = new dynamoose.Schema({
 	id: String,
 	age: Number,
-});
+})
 
 // Create a model
 
-const people = dynamoose.model('people', schema);
+const people = dynamoose.model('people', schema)
 
 export const handler = async event => {
 	// create a response object
 	const response = {
 		statusCode: null,
 		body: null,
-	};
+	}
 	// Check if searching by ID or by nothing
-	console.log('EVENT PATH PARAMETERS:', event.pathParameters);
+	console.log('EVENT PATH PARAMETERS:', event.pathParameters)
 	// get the path parameters no matter the state they are in to avoid errors
 	let eventPath =
 		event.pathParameters && event.pathParameters.id
 			? event.pathParameters.id
-			: null;
-	console.log('\n\nEVENT PATH: ', eventPath);
+			: null
+	console.log('\n\nEVENT PATH: ', eventPath)
 
 	try {
-		let results;
+		let results
 
 		// Get ALL if there is no event path
 		if (!eventPath) {
-			console.log('\n\nGETTING ALL:');
+			console.log('\n\nGETTING ALL:')
 			// scan the database
-			results = await people.scan().exec();
-			console.log('GET ALL RESULTS:', results);
+			results = await people.scan().exec()
+			console.log('GET ALL RESULTS:', results)
 		} else {
-			console.log('\n\nGETTING ONE:');
+			console.log('\n\nGETTING ONE:')
 			// scan the database by ID
-			results = await people.get(`${eventPath}`);
-			console.log('GET ONE RESULTS:', results);
+			results = await people.get(`${eventPath}`)
+			console.log('GET ONE RESULTS:', results)
 		}
 
 		// stringify results because amazon wants body stringified
-		response.body = JSON.stringify(results);
-		response.statusCode = 200;
+		response.body = JSON.stringify(results)
+		response.statusCode = 200
 	} catch (err) {
-		console.log(err);
-		response.body = JSON.stringify('ERROR AT GET:', err);
-		response.statusCode = 500;
+		console.log(err)
+		response.body = JSON.stringify('ERROR AT GET:', err)
+		response.statusCode = 500
 	}
 
-	console.log('FINAL RESPONSE:', response);
+	console.log('FINAL RESPONSE:', response)
 
-	return response;
-};
+	return response
+}
